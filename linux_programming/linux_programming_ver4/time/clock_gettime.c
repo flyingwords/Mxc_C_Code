d8470d8997435fea347fe7c978f6ecffe3868194
flyingwords <maxucan163@163.com>
2021-05-13 15:11:50 +0800

    cm

--- linux_programming/linux_programming_ver4/time/clock_gettime.c
+++ linux_programming/linux_programming_ver4/time/clock_gettime.c
@@ -15,11 +15,10 @@ int main(int argc, char **argv)
 	struct timespec time2 = {0, 0};
 	
 	float temp;
- 
 	clock_gettime(CLOCK_REALTIME, &time1);		
 	sleep(2);
 	clock_gettime(CLOCK_REALTIME, &time2);	 
-	temp = (time2.tv_nsec - time1.tv_nsec) / 1000000;
+	temp = (time2.tv_nsec - time1.tv_nsec) / 1000000 + (time2.tv_sec - time1.tv_sec) * 1000;
 	printf("time = %f ms\n", temp);
 	return 0;
 }

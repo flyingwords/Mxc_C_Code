246090bd2f5fa8507a0def17b5d0cb7179be48f1
flyingwords <maxucan163@163.com>
2021-06-17 21:06:33 +0800

    cm

---
+++ makefile_test/test2/lib_api/c_file_2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int func2(void){
+	printf("hello man\n");
+	return 0;
+}
+
---
+++ makefile_test/test2/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "c_file_1.h"
+#include "c_file_2.h"
+int main()
+{
+	func();
+	func2();
+	return 0;
+}
